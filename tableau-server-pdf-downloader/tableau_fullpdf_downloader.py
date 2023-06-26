fd0a6c9911fea309deb4709e9a3b2bc254ad118c
Jmac <jonathan.macdonald@theinformationlab.co.uk>
2023-06-26 16:18:45 +0100

    Added different auth methods

--- tableau-server-pdf-downloader/tableau_fullpdf_downloader.py
+++ tableau-server-pdf-downloader/tableau_fullpdf_downloader.py
@@ -14,10 +14,23 @@ def prompt_user():
     data = {}
     # Ask user for server URL
     data["server_url"] = input("Please enter server URL: ")
-    # Ask user for username
-    data["username"] = input("Please enter your username: ")
-    # Ask user for password (password will be hidden)
-    data["password"] = getpass.getpass("Please enter your password: ")
+    # Ask user for authentication method
+    data["auth_method"] = input(
+        "Please enter authentication method (password or token): ")
+    if data["auth_method"] == "password":
+        # Ask user for username
+        data["username"] = input("Please enter your username: ")
+        # Ask user for password (password will be hidden)
+        data["password"] = getpass.getpass("Please enter your password: ")
+    elif data["auth_method"] == "token":
+        # Ask user for token name
+        data["token_name"] = input("Please enter your token name: ")
+        # Ask user for token value
+        data["token_value"] = getpass.getpass(
+            "Please enter your token value: ")
+    else:
+        print("Invalid authentication method. Please choose either 'password' or 'token'.")
+        sys.exit(1)
     # Ask user for site name
     data["site_name"] = input("Please enter the site name: ")
     # Ask user for project name
@@ -71,8 +84,6 @@ def main():
 
     # Assign variables to data from user or JSON file
     server_url = data["server_url"]
-    username = data["username"]
-    password = data["password"]
     site_name = data["site_name"]
     project_name = data["project_name"]
     workbook_name = data["workbook_name"]
@@ -83,7 +94,13 @@ def main():
     filter_value = data["filter_value"]
 
     # Initialize Tableau authentication and server
-    tableau_auth = TSC.TableauAuth(username, password, site_name)
+    if data["auth_method"] == "password":
+        tableau_auth = TSC.TableauAuth(
+            data["username"], data["password"], site_name)
+    else:  # if data["auth_method"] == "token":
+        tableau_auth = TSC.PersonalAccessTokenAuth(
+            data["token_name"], data["token_value"], site_name)
+
     server = TSC.Server(server_url, use_server_version=True)
 
     # Set PDF request options
@@ -91,45 +108,51 @@ def main():
     pdf_request_options.vf(filter_field, filter_value)  # Apply filter
 
     # Begin server authentication
-    with server.auth.sign_in(tableau_auth):
-        all_projects, _ = server.projects.get()  # Get all projects
-        # Get project specified by user or JSON file
-        project = next(
-            (project for project in all_projects if project.name == project_name), None)
+    try:
+        with server.auth.sign_in(tableau_auth):
+            all_projects, _ = server.projects.get()  # Get all projects
+            # Get project specified by user or JSON file
+            project = next(
+                (project for project in all_projects if project.name == project_name), None)
 
-        if not project:
-            # If specified project not found, display error
-            print(f"No project named '{project_name}' found.")
-            return
+            if not project:
+                # If specified project not found, display error
+                print(f"No project named '{project_name}' found.")
+                return
 
-        # Create request options for workbook
-        req_option_workbook = TSC.RequestOptions()
-        req_option_workbook.filter.add(TSC.Filter(TSC.RequestOptions.Field.ProjectName,
-                                                  TSC.RequestOptions.Operator.Equals,
-                                                  project_name))  # Add filter for project name
+            # Create request options for workbook
+            req_option_workbook = TSC.RequestOptions()
+            req_option_workbook.filter.add(TSC.Filter(TSC.RequestOptions.Field.ProjectName,
+                                                      TSC.RequestOptions.Operator.Equals,
+                                                      project_name))  # Add filter for project name
 
-        all_workbooks, _ = server.workbooks.get(
-            req_option_workbook)  # Get all workbooks
-        for wb in all_workbooks:
-            # If workbook name matches the one specified by user or JSON file
-            if wb.name.lower() == workbook_name.lower():
-                all_views, _ = server.views.get()  # Get all views
-                with PdfMerger() as merger:  # Begin PDF merge
-                    for view in all_views:
-                        if view.workbook_id == wb.id:  # If view belongs to the specified workbook
-                            # Populate PDF for the view
-                            server.views.populate_pdf(
-                                view, pdf_request_options)
-                            # Set path for the PDF
-                            pdf_path = os.path.join(
-                                output_path, f"{view.name}.pdf")
-                            with open(pdf_path, "wb") as f:  # Open PDF file
-                                f.write(view.pdf)  # Write PDF data to file
-                            merger.append(pdf_path)  # Append PDF to the merger
-                            # Deletes the individual PDF file after merging
-                            os.remove(pdf_path)
-                    # Write merged PDF file to specified output path
-                    merger.write(os.path.join(output_path, output_filename))
+            all_workbooks, _ = server.workbooks.get(
+                req_option_workbook)  # Get all workbooks
+            for wb in all_workbooks:
+                # If workbook name matches the one specified by user or JSON file
+                if wb.name.lower() == workbook_name.lower():
+                    all_views, _ = server.views.get()  # Get all views
+                    with PdfMerger() as merger:  # Begin PDF merge
+                        for view in all_views:
+                            if view.workbook_id == wb.id:  # If view belongs to the specified workbook
+                                # Populate PDF for the view
+                                server.views.populate_pdf(
+                                    view, pdf_request_options)
+                                # Set path for the PDF
+                                pdf_path = os.path.join(
+                                    output_path, f"{view.name}.pdf")
+                                with open(pdf_path, "wb") as f:  # Open PDF file
+                                    f.write(view.pdf)  # Write PDF data to file
+                                # Append PDF to the merger
+                                merger.append(pdf_path)
+                                # Deletes the individual PDF file after merging
+                                os.remove(pdf_path)
+                        # Write merged PDF file to specified output path
+                        merger.write(os.path.join(
+                            output_path, output_filename))
+    except Exception as e:
+        print(f"Authentication failed. Please verify your credentials and try again.")
+        sys.exit(1)
 
 
 # Call the main function when the script is run
